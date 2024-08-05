e5d98fbc7a981a6176c8a4d7d71baefc04ab4e3d
ricardo <110841176+sicanet-ricardo@users.noreply.github.com>
2024-08-04 23:34:07 -0600

    Update medical.module.ts

--- admin_clinica/src/app/medical/medical.module.ts
+++ admin_clinica/src/app/medical/medical.module.ts
@@ -4,16 +4,16 @@ import { CommonModule } from '@angular/common';
 import { MedicalRoutingModule } from './medical-routing.module';
 import { MedicalComponent } from './medical.component';
 import { SharedModule } from '../shared/shared.module';
-import { SidebarComponent } from '../common-component/sidebar/sidebar.component';
+//import { SidebarComponent } from '../common-component/sidebar/sidebar.component';
 //import { HeaderComponent } from '../common-component/header/header.component';
-import { ModalComponent } from '../core/modal/modal.component';
+//import { ModalComponent } from '../core/modal/modal.component';
 
 
 @NgModule({
   declarations: [
     MedicalComponent,
     //HeaderComponent,
-    SidebarComponent,
+    //SidebarComponent,
   ],
   imports: [
     CommonModule,
